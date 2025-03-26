bb7f1c7c04b46196ddd66d811d43b8314774adf4
Chris-Wels <Christianwelsh43@gmail.com>
2025-03-26 21:31:52 +1300

    Server

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,12 @@
-
+//Use for serveless vercel deployment
 const path = require('path');
 const express = require('express');
-//const cors = require('cors');
 const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5503;  // Use Render's port in production, default to 5503 locally
 
-
-// app.use(cors({
-
-// })); // Allow frontend to access backend
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'Public', 'index.html'));
-});
 // Serve static files from the 'public' folder
-//app.use(express.static(path.join(__dirname, 'Public')));
 app.use(express.static(path.resolve(__dirname, 'Public')));
 
 // Weather API route
@@ -34,8 +24,51 @@ app.get('/api/weather', async (req, res) => {
     }
 });
 
-// Start server
-app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+// Return the app as a serverless function
+module.exports = app;
+
+
+
+
+
+//Use for Local Host testing
+// const path = require('path');
+// const express = require('express');
+// //const cors = require('cors');
+// const axios = require('axios');
+// require('dotenv').config();
+
+// const app = express();
+// const port = process.env.PORT || 5503;  // Use Render's port in production, default to 5503 locally
+
+
+// // app.use(cors({
+
+// // })); // Allow frontend to access backend
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'Public', 'index.html'));
+// });
+// // Serve static files from the 'public' folder
+// //app.use(express.static(path.join(__dirname, 'Public')));
+// app.use(express.static(path.resolve(__dirname, 'Public')));
+
+// // Weather API route
+// app.get('/api/weather', async (req, res) => {
+//     const city = req.query.city;
+//     if (!city) return res.status(400).json({ error: 'City is required' });
+
+//     try {
+//         const apiKey = process.env.WEATHER_API_KEY;
+//         const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?q=${city}&days=7&key=${apiKey}`);
+//         res.json(response.data);
+//     } catch (error) {
+//         console.error('Error fetching weather data:', error);
+//         res.status(500).json({ error: 'Failed to fetch weather data' });
+//     }
+// });
+
+// // Start server
+// app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
 
 
 
